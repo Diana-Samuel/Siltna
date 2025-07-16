c319514b9adcf0ca1095ad8450ba75e717414f9f
Diana Samuel <diana.samuel.7110@gmail.com>
2025-07-16 15:35:32 +0300

    added some changes to Alg

--- algorithm.py
+++ algorithm.py
@@ -6,30 +6,43 @@ def postArrange(posts: list,userid: str) -> list:
     newPosts = {}
     for post in posts:
         score = 0
-        postid = post[0]
-        posterid = post[1]
-        text = post[2]
-        images = post[3]
-        status = post[-3]
-        date = post[-2]
-        tags = post[-1]
+        postid = post["postId"]
+        posterid = post["posterId"]
+        text = post["text"]
+        images = post["images"]
+        status = post["status"]
+        date = post["date"]
+        tags = post["tags"]
+
         interests = db.getInterests(userid)
         if not status:
             continue
+
         userInfo = db.getuserinfo(userid)
+
         following = userInfo[-1]
+
         if posterid in following:
             score += 50
+
         if "text" in interests and len(text) > 0:
             score += 10
+
         if "image" in interests and len(images) > 0:
             score += 10
+
         timenow = datetime.datetime.now(datetime.UTC)
+
         diffInDays = (timenow.date() - date).days
+
         score -= diffInDays * 2
+        
         newPosts[postid] = score
+
     newPosts = sorted(newPosts)
     newnewPosts = []
+
     for i in newPosts:
         newnewPosts.append(db.getPost(i)[1])
+    
     return newnewPosts
